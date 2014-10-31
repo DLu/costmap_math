e56d6f564358af6f68f8df754793949b250e4cd8
David V. Lu!! <davidvlu@gmail.com>
2014-10-31 14:38:39 -0500

    Draw Total PCost

--- pcost.py
+++ pcost.py
@@ -7,7 +7,7 @@ N = 500
 def f(x,y):
     return gaussian(x,y, dy=-N/2, A=50)
 
-xs = range(0,50)
+xs = range(0,30)
 As = []
 bs = []
 cs = []
@@ -18,13 +18,14 @@ for x in xs:
     c = sum([f(x,y) for y in range(N+1)])
     As.append(a)
     bs.append(b)
-    cs.append(c)
+    cs.append(a+c)
     v = a + b + c
     ys.append(v)
     
-plot(xs, As, label="A")
-plot(xs, bs, label="B")
-plot(xs, cs, label="C")
-plot(xs, ys, label="Y")
-legend()
+#plot(xs, As, label="A")
+plot(xs, bs, label="Step cost")
+plot(xs, cs, label="Gaussian cost")
+plot(xs, ys, label="Total cost")
+ylim([0, 2500])
+legend(loc=2)
 show()
