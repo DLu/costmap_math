40bf17ae99752445e7de97359c22a65b2069ee8c
David V. Lu!! <davidvlu@gmail.com>
2014-10-31 14:38:50 -0500

    Add svenstrup model

--- functions.py
+++ functions.py
@@ -10,8 +10,10 @@ def none(x, y, dx=0, dy=0):
 
 CMAP_none = (none, {})
 
-def gaussian(x, y, dx=0, dy=0, A=99, var=100):
+def gaussian(x, y, dx=0, dy=0, A=99, var=100, var2=None):
     bv = none(x,y,dx,dy)
+    if var2 is None:
+        var2 = var
     x += dx
     y += dy
     h = sqrt(x*x+y*y)
@@ -19,7 +21,7 @@ def gaussian(x, y, dx=0, dy=0, A=99, var=100):
     mx = cos(angle) * h
     my = sin(angle) * h
     f1 = pow(mx, 2.0)/(2.0 * var)
-    f2 = pow(my, 2.0)/(2.0 * var)
+    f2 = pow(my, 2.0)/(2.0 * var2)
     return max(A * exp(-(f1 + f2)), bv)
     
 CMAP_gaussian = (gaussian, {'A':[0,100,99], 'var':[1,400,100]})
@@ -28,6 +30,14 @@ def two_gaussians(x, y, dx=0, dy=0, A1=50, A2=40, var1=100, var2=100):
     return max(gaussian(x,y,dx,dy,A1,var1) , gaussian(x,y,dx,dy,A2,var2))
 
 CMAP_two_gaussians = (two_gaussians, {'A1':[0,100,50], 'var1':[1,400,100], 'A2':[0,100,40], 'var2':[1,400,100]})
+
+def svenstrup(x,y,dx=0,dy=0):
+    A = -gaussian(x,y,dx,dy,var=75)
+    B = 0
+    if y > -dy:
+        B = gaussian(x,y,dx,dy,var=20,var2=10)
+    return A + B
+
 
 def linear(x, y, dx=0, dy=0, A=99, slope=1, power=2):
     x += dx
@@ -56,3 +66,19 @@ def constant(x, y, dx=0, dy=0, A=50, xd=5, yd=5):
 CMAP_constant = (constant, {'A': [0, 100, 20], 'xd': [0, 20, 5], 'yd': [0, 50, 5]})
 
 
+
+def gaussianc(x, y, dx=0, dy=0, A=99, var=100, var2=None, c=.1):
+    bv = 0 * none(x,y,dx,dy)
+    if var2 is None:
+        var2 = var
+    x += dx
+    y += dy
+    h = sqrt(x*x+y*y)
+    angle = atan2(y,x)
+    mx = cos(angle) * h
+    my = sin(angle) * h
+    f1 = pow(mx, 2.0)/(2.0 * var)
+    f2 = pow(my, 2.0)/(2.0 * var2)
+    return max(A * exp(-(f1 + f2)), bv) + c
+    
+CMAP_gaussianc = (gaussianc, {'A':[0,100,99], 'var':[1,400,100], 'c':[0,10,10]})
